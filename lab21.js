0f190be296915f70d9b18d36d12f778effa1fb03
ctidwell21 <62309982+ctidwell21@users.noreply.github.com>
2020-04-30 15:01:43 -0700

    Add files via upload

---
+++ lab21.js
@@ -0,0 +1,12 @@
+// /**
+//  * Author: Christian Tidwell <ctidwell@csumb.edu>
+//  * Created:  April, 29 2020
+//  * License: Public Domain
+//  * (c) Copyright by Christian Tidwell from CST 252: Scripting for Multimedia 
+//  **/
+
+$(document).ready(function() {
+    $("button").click(function(body) {
+    	console.log(body); 
+    	  });
+});
